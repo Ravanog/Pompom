9a28c05579e5abdf754cd638a0a86493dbb37d09
Rohith <149065101+Ravanog@users.noreply.github.com>
2026-07-24 11:55:13 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -71,9 +71,18 @@ async def start_command(client: Client, message: Message):
             "<b>⛔️ You are Bᴀɴɴᴇᴅ from using this bot.</b>\n\n"
             "<i>Contact support if you think this is a mistake.</i>",
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Contact Support", url=BAN_SUPPORT)]]
-            )
-        )
+                InlineKeyboardButton('ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ ⁠✿', url='https://t.me/Hari_Moviez')
+            ],[
+            InlineKeyboardButton('ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ ⁠✷', url='https://t.me/iPopcornMoviesGroups'),
+            InlineKeyboardButton('ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ 2 ⁠◕', url='https://t.me/Hari_Searchx')
+            ],[
+            InlineKeyboardButton('ғɪʟᴇꜱ ᴄʜᴀɴɴᴇʟ ⁠●', url='https://t.me/+Fey8agWUFMBkNDk1'),
+            InlineKeyboardButton('ғɪʟᴇꜱ ᴄʜᴀɴɴᴇʟ 2', url='https://t.me/hari_kannada_cinema')
+            ],[
+            InlineKeyboardButton('💁‍♀️ ʜᴇʟᴘ', callback_data='help'),
+            InlineKeyboardButton('😊 ᴀʙᴏᴜᴛ', callback_data='about')
+            ],[
+            InlineKeyboardButton('ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ 2 ⁠✿', url='https://t.me/TG_BOTS_UPDATE')]))
 
     # File auto-delete time in seconds
     FILE_AUTO_DELETE = await db.get_del_timer()
