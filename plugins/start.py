42ed7d467fcd57cddf32759028bc161021b87c72
Rohith <149065101+Ravanog@users.noreply.github.com>
2026-07-24 12:22:21 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -224,7 +224,7 @@ async def start_command(client: Client, message: Message):
     ]
         )
         await message.reply_photo(
-            photo=START_PIC,
+            photo=random.choice(START_PIC),
             caption=START_MSG.format(
                 first=message.from_user.first_name,
                 last=message.from_user.last_name,
